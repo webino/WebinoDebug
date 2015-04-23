8add5abc87b8ad2a686142b13352012538cc4a4f
Peter Bacinsky <peter@bacinsky.sk>
2015-04-24 00:12:19 +0200

    Updated ModuleOptionsFactory + test , use constants

--- src/WebinoDebug/Factory/ModuleOptionsFactory.php
+++ src/WebinoDebug/Factory/ModuleOptionsFactory.php
@@ -3,7 +3,7 @@
  * Webino (http://webino.sk/)
  *
  * @link        https://github.com/webino/WebinoDebug/ for the canonical source repository
- * @copyright   Copyright (c) 2014 Webino, s. r. o. (http://webino.sk/)
+ * @copyright   Copyright (c) 2014-2015 Webino, s. r. o. (http://webino.sk/)
  * @license     BSD-3-Clause
  */
 
@@ -19,11 +19,17 @@ use Zend\ServiceManager\ServiceLocatorInterface;
 class ModuleOptionsFactory implements FactoryInterface
 {
     /**
+     * Configuration service section key
+     */
+    const CONFIG_KEY = 'webino_debug';
+
+    /**
+     * @param ServiceLocatorInterface $services
      * @return ModuleOptions
      */
     public function createService(ServiceLocatorInterface $services)
     {
         $config = $services->get('Config');
-        return new ModuleOptions(!empty($config['webino_debug']) ? $config['webino_debug'] : []);
+        return new ModuleOptions(empty($config[$this::CONFIG_KEY]) ? [] : $config[$this::CONFIG_KEY]);
     }
 }
