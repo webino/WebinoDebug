c7da3b5af00e39f1038d3b161c12bc6d69599025
Peter Bacinsky <peter@bacinsky.sk>
2018-06-04 14:15:09 +0200

    Fixing debugger script.js

--- data/assets/Debugger/script.js
+++ data/assets/Debugger/script.js
@@ -1,12 +1,27 @@
-setTimeout(function () {
-    // custom bar title
-    document.getElementById('tracy-debug-logo').title = document.getElementById('webino-debug-bar-meta').getAttribute('data-bar-title');
+(function () {
+    var init = function () {
+        // init wait
+        setTimeout(function () {
+            // resolve meta data
+            var meta = document.getElementById('webino-debug-bar-meta');
+            if (!meta) {
+                init();
+                return;
+            }
 
-    // panel to float width fix
-    var PanelToFloat = Tracy.DebugPanel.prototype.toFloat;
-    Tracy.DebugPanel.prototype.toFloat = function () {
-        PanelToFloat.call(this);
-        this.elem.style.width = this.elem.offsetWidth + 'px';
-        this.elem.style.height = this.elem.offsetHeight + 'px';
+            // custom bar title
+            document.getElementById('tracy-debug-logo').title = meta.getAttribute('data-bar-title');
+
+            // panel to float width fix
+            var PanelToFloat = Tracy.DebugPanel.prototype.toFloat;
+            Tracy.DebugPanel.prototype.toFloat = function () {
+                PanelToFloat.call(this);
+                this.elem.style.width = this.elem.offsetWidth + 'px';
+                this.elem.style.height = this.elem.offsetHeight + 'px';
+            };
+
+        }, 1);
     };
-}, 1);
+
+    init();
+})();
